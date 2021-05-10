d26ace36f3ce9d74c84f13c7d2246474b4c9ae5d
Butter Fish <sun1231010@gmail.com>
2021-05-10 08:20:58 +0700

    Starting to code (for real)

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-gfvhfvyhvh
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Play</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+<?php
+    echo "Hello world";
+?>
+</body>
+</html>
