3636feb83b4d773b059874b0d419d1fea8ca195c
Butter Fish <sun1231010@gmail.com>
2021-05-13 07:47:25 +0700

    temporary change

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,10 +5,76 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Google Play</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900"/>
+    <link rel="stylesheet" href="/style.css"/>
 </head>
 <body>
-<?php
-    echo "Hello world";
-?>
+    <div class="top">
+        <div class="divide">
+            <h2 id="web-title">Google Play</h2>
+            <div class="searchbar" id="form-outline">
+                <div class="form-outline">
+                    <input type="search" id="search-input" class="form-control" placeholder="Search..."/>
+                </div>
+                <button type="button" class="search-btn" id="search-btn">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div><br/></br>
+        <table id="top-games">
+            <tr>
+            </tr>
+            <tr>
+                <td colspan="4">Top games</td>
+            </tr>
+            <tr>
+                <td width="170px">Cột trái</td>
+                <td width="170px">Cột giữa 1</td>
+                <td width="170px">Cột giữa 2</td>
+                <td width="170px">Cột phải</td>
+            </tr>
+        </table><br/><br/>
+        <table id="top-games">
+            <tr>
+            </tr>
+            <tr>
+                <td colspan="4">Top games</td>
+            </tr>
+            <tr>
+                <td width="170px">Cột trái</td>
+                <td width="170px">Cột giữa 1</td>
+                <td width="170px">Cột giữa 2</td>
+                <td width="170px">Cột phải</td>
+            </tr>
+        </table><br/><br/>
+        <table id="top-games">
+            <tr>
+            </tr>
+            <tr>
+                <td colspan="4">Top games</td>
+            </tr>
+            <tr>
+                <td width="170px">Cột trái</td>
+                <td width="170px">Cột giữa 1</td>
+                <td width="170px">Cột giữa 2</td>
+                <td width="170px">Cột phải</td>
+            </tr>
+        </table><br/><br/>
+        <table id="top-games">
+            <tr>
+            </tr>
+            <tr>
+                <td colspan="4">Top games</td>
+            </tr>
+            <tr>
+                <td width="170px">Cột trái</td>
+                <td width="170px">Cột giữa 1</td>
+                <td width="170px">Cột giữa 2</td>
+                <td width="170px">Cột phải</td>
+            </tr>
+        </table><br/><br/>
+    </div>
 </body>
 </html>
